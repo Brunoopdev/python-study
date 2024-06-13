c8ffeda96f67b359a1487ea6b766fa3e177c0f7e
Bruno <gpaes247@gmail.com>
2024-06-12 23:43:02 -0300

    primeiro commit

---
+++ test.py
@@ -0,0 +1,27 @@
+# Exibe números presentes entre dois outros números
+
+# num1 = int(input('Digite um número inteiro\n'));
+# num2 = int(input('Digite outro número inteiro\n'));
+# print('');
+
+# contador = 0;
+
+# for contador in range(num1, num2 + 1):
+#     print(f'{contador}');
+
+
+#  Dias das colônias
+
+# colonia A cresce 3% e inicia com 4.
+# colonia B cresce 1,5% e inicia com 10.
+
+# coloniaA = 4;
+# coloniaB = 10;
+# dias = 0;
+
+# while coloniaA <= coloniaB:
+#         coloniaA += coloniaA * 0.03;
+#         coloniaB += coloniaB * 0.015;
+#         dias += 1;
+# print(f'Levou exatos: {dias} dias');
+
